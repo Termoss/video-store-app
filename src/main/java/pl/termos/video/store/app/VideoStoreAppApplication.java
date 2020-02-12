0a57af984551af48137415a751cc61b062a5544f
tomas <tomaszczerwiec1@gmail.com>
2020-02-13 00:18:03 +0100

    Initial commit

---
+++ src/main/java/pl/termos/video/store/app/VideoStoreAppApplication.java
@@ -0,0 +1,13 @@
+package pl.termos.video.store.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStoreAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/termos/video/store/app/VideoStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.termos.video.store.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
